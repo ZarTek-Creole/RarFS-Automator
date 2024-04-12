5e17eba70b33b0674c5a04b0d1a0561cdfd9483a
ZarTek Creole <zartek.creole@gmail.com>
2024-04-12 13:27:38 +0200

    ✨ fonctionnalité(install_rarfs.sh) : améliorer le script d'installation pour une meilleure gestion des erreurs et une configuration plus flexible de rar2fs ♻️ refactor(install_rarfs.sh) : utiliser une variable WORK_DIR globale pour simplifier la gestion des répertoires de travail 🔧 fonctionnalité(install_rarfs.sh) : ajouter des vérifications explicites et des messages d'erreur pour chaque étape critique du processus d'installation ✅ fonctionnalité(install_rarfs.sh) : assurer la compilation et l'installation de la bibliothèque UnRAR dans un répertoire spécifié pour une meilleure intégration avec rar2fs

--- install_rarfs.sh
+++ install_rarfs.sh
@@ -12,7 +12,12 @@ readonly REPO="hasse69/rar2fs"
 readonly UNRAR_VERSION="6.2.1"
 readonly API_URL="https://api.github.com/repos/$REPO/releases/latest"
 readonly DEPENDENCIES="make gcc g++ libfuse-dev unzip ccache"
-readonly NUM_CORES=$(nproc)  # Automatically determines the number of cores
+# shellcheck disable=SC2155
+readonly NUM_CORES=$(nproc)
+# shellcheck disable=SC2155
+readonly WORK_DIR=$(mktemp -d)
+readonly RARFS_CONFIGURATION_ARGS="--with-unrar-lib=/usr/local/lib --with-unrar=${WORK_DIR}/unrar"
+
 
 # Fetches the latest version tag and download URL from GitHub
 fetch_latest_version_and_url() {
@@ -32,36 +37,79 @@ fetch_latest_version_and_url() {
 
 # Downloads and installs UnRAR
 install_unrar() {
-  local work_dir="$1"
   echo "Installing UnRAR..."
-  wget -q "https://www.rarlab.com/rar/unrarsrc-${UNRAR_VERSION}.tar.gz" -P "$work_dir" || return 1
-  tar -zxf "$work_dir/unrarsrc-${UNRAR_VERSION}.tar.gz" -C "$work_dir" || return 1
-  pushd "$work_dir/unrar" > /dev/null || return 1
-  make -s -j"$NUM_CORES" || { echo "Compilation of unrar failed" >&2; return 1; }
-  sudo make install || { echo "Installation of unrar failed" >&2; return 1; }
-  make -s lib || { echo "Compilation of library failed" >&2; return 1; }
-  sudo make install-lib || { echo "Library installation of unrar failed" >&2; return 1; }
-  popd > /dev/null
+  
+  # Téléchargement du source
+  wget -q "https://www.rarlab.com/rar/unrarsrc-${UNRAR_VERSION}.tar.gz" -P "$WORK_DIR" || { echo "Failed to download UnRAR source."; return 1; }
+  
+  # Extraction du tar.gz
+  tar -zxf "$WORK_DIR/unrarsrc-${UNRAR_VERSION}.tar.gz" -C "$WORK_DIR" || { echo "Failed to extract UnRAR source."; return 1; }
+
+  # Changement au dossier de compilation
+  pushd "$WORK_DIR/unrar" > /dev/null || { echo "UnRAR source directory does not exist."; return 1; }
+  
+  # Compilation du programme
+  echo "Compiling UnRAR..."
+  make -s -j"$NUM_CORES" || { echo "Compilation of unrar failed"; return 1; }
+  echo "UnRAR compiled successfully."
+  
+  # Installation du programme
+  sudo make install || { echo "Installation of unrar failed"; return 1; }
+  echo "UnRAR installed successfully."
+  
+  # Compilation et installation de la librairie
+    echo "Compiling UnRAR library..."
+    make -s lib || { echo "Compilation of library failed"; return 1; }
+    sudo make install-lib DESTDIR=/usr/local || { echo "Library installation of unrar failed"; return 1; }
+    echo "UnRAR library installed successfully."
+  
+  popd > /dev/null || return 1
 }
 
 # Downloads and installs rar2fs
 install_rar2fs() {
-  local work_dir="$1"
-  local download_url="$2"
+  local download_url="$1"
   echo "Installing rar2fs..."
-  wget -q "$download_url" -P "$work_dir" || return 1
+  
+  # Téléchargement du fichier tar.gz
+  wget -q "$download_url" -P "$WORK_DIR" || { echo "Failed to download rar2fs source."; return 1; }
+  
+  # Extraction du tar.gz
   local version_dir
-  version_dir=$(basename "$download_url" .tar.gz)
-  tar -zxf "$work_dir/$version_dir.tar.gz" -C "$work_dir" || return 1
-  pushd "$work_dir/$version_dir" > /dev/null || return 1
-  ./configure --with-unrar-lib="/usr/lib/" > /dev/null 2>&1 && make -s -j"$NUM_CORES" && sudo make install > /dev/null 2>&1 || { echo "Installation failed" >&2; return 1; }
-  popd > /dev/null
+  version_dir="$(basename "$download_url" .tar.gz)"
+  tar -zxf "$WORK_DIR/$version_dir.tar.gz" -C "$WORK_DIR" || { echo "Failed to extract rar2fs source."; return 1; }
+  
+  # Changement au dossier de compilation
+  pushd "$WORK_DIR/$version_dir" > /dev/null || { echo "rar2fs source directory does not exist."; return 1; }
+  
+  # Configuration et compilation
+  # shellcheck disable=SC2086
+  if ! ./configure $RARFS_CONFIGURATION_ARGS > /dev/null; then
+    echo "Configuration of rar2fs failed."
+    return 1
+  fi
+  
+  if ! make -s -j"$NUM_CORES"; then
+    echo "Compilation of rar2fs failed."
+    return 1
+  fi
+  
+  # Installation
+  if ! sudo make install > /dev/null; then
+    echo "Installation of rar2fs failed."
+    return 1
+  fi
+  
+  echo "rar2fs installed successfully."
+  popd > /dev/null || return 1
 }
 
 # Install necessary dependencies
 install_dependencies() {
     echo "Installing dependencies..."
-    sudo apt-get update -qq && sudo apt-get install -y $DEPENDENCIES > /dev/null || return 1
+    sudo apt-get update -qq \
+        && sudo apt-get install -y $DEPENDENCIES > /dev/null \
+        || { echo "Failed to install dependencies."; return 1; }
     echo "Dependencies installed."
 }
 
@@ -76,10 +124,10 @@ configure_ccache() {
 
 # Manages the installation process
 main() {
-  local work_dir version download_url
+  local version download_url
 
-  work_dir=$(mktemp -d)
-  trap 'rm -rf "$work_dir"' EXIT  # Clean up on script exit
+  
+  trap 'rm -rf "$WORK_DIR"' EXIT  # Clean up on script exit
 
   install_dependencies || {
     echo "Failed to install dependencies." >&2
@@ -88,17 +136,17 @@ main() {
 
   configure_ccache
 
-  install_unrar "$work_dir" || {
+  install_unrar || {
     echo "UnRAR installation failed." >&2
     exit 1
   }
 
-  read version download_url < <(fetch_latest_version_and_url) || exit 1
-  install_rar2fs "$work_dir" "$download_url" || {
-    echo "rar2fs installation failed." >&2
+  read -r version download_url < <(fetch_latest_version_and_url) || exit 1
+  install_rar2fs "$download_url" || {
+    echo "rar2fs $version installation failed." >&2
     exit 1
   }
-  echo "rar2fs installation completed successfully."
+  echo "rar2fs $version installation completed successfully."
 }
 
 main
